08c141016a0bdd7a4c6dbf9f58153f612fc48d08
Nguyen Tram <ngoctrambu13012001@gmail.com>
2022-07-25 09:48:05 +0700

    product2

--- js/data.js
+++ js/data.js
@@ -3,84 +3,84 @@ const product= [
         id:1,
         name: 'Anxi Tie Guan Yin Tea',
         price: 64.27,
-        img:'../img/product/Anxi Tie Guan Yin Tea.jpg',
+        img:'./img/product/Anxi Tie Guan Yin Tea.jpg',
 
     },
     {
         id:2,
         name: 'Black Unpzessed Tea',
         price: 28.00,
-        img:'../img/product/Black Unpzessed Tea.jpg',
+        img:'./img/product/Black Unpzessed Tea.jpg',
 
     },
     {
         id:3,
         name: 'Green Snail Spring Tea',
         price: 95.21,
-        img:'../img/product/Green Snail Spring Tea.jpg',
+        img:'./img/product/Green Snail Spring Tea.jpg',
 
     },
     {
         id:4,
         name: 'Hezb Mazzubium Vulgaze',
         price: 45.00,
-        img:'../img/product/Hezb Mazzubium Vulgaze.jpg',
+        img:'./img/product/Hezb Mazzubium Vulgaze.jpg',
 
     },
     {
         id:5,
         name: 'Hezb Mentha Spicata',
         price: 8.00,
-        img:'../img/product/Hezb Mentha Spicata.jpg',
+        img:'./img/product/Hezb Mentha Spicata.jpg',
 
     },
     {
         id:6,
         name: 'Hezb Oziganum Vulgaze',
         price: 35.9,
-        img:'../img/product/Hezb Oziganum Vulgaze.jpg',
+        img:'./img/product/Hezb Oziganum Vulgaze.jpg',
 
     },
     {
         id:7,
         name: 'Hibiscus Calyces',
         price: 24.00,
-        img:'../img/product/Hibiscus Calyces.jpg',
+        img:'./img/product/Hibiscus Calyces.jpg',
 
     },
     {
         id:8,
         name: 'Jasmin Flowering Tea',
         price: 30.00,
-        img:'../img/product/Jasmin Flowering Tea.jpg',
+        img:'./img/product/Jasmin Flowering Tea.jpg',
 
     },
     {
         id:9,
         name: 'Jasmine Scented Green Tea',
         price: 30.00,
-        img:'../img/product/Jasmine Scented Green Tea.jpg',
+        img:'./img/product/Jasmine Scented Green Tea.jpg',
 
     },
     {
         id:10,
         name: 'Jasmine Scented Red Tea',
         price: 110.00,
-        img:'../img/product/Jasmine Scented Red Tea.jpg',
+        img:'./img/product/Jasmine Scented Red Tea.jpg',
 
     },
     {
         id:11,
         name: 'Mate Tea With Monazda',
         price: 123.05,
-        img:'../img/product/Mate Tea With Monazda.jpg',
+        img:'./img/product/Mate Tea With Monazda.jpg',
 
     },
     {
         id:12,
         name: 'Oragnic Green Tea',
         price: 90.24,
-        img:'../img/product/Oragnic Green Tea.jpg',
+        img:'./img/product/Oragnic Green Tea.jpg',
 
     },
     
